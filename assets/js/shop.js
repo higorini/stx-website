64218cf59bc9512a8ee3f5c7ca131d366619fa63
higorini <108697425+higorini@users.noreply.github.com>
2024-05-17 10:40:34 +0100

    feat: muita coisa, desculpa

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,29 +1,125 @@
 function fillProductDetails(product) {
-  let productNameElement = document.querySelector(".product p");
-  let productGalleryElement = document.querySelector(".product__gallery");
-  let productDescriptionImageElement = document.querySelector(
-    ".description__image"
-  );
-  let productDescriptionTextElement =
-    document.querySelector(".description__text");
-  let productBuyElement = document.querySelector(".product__buy");
+  // Query selectors
+  let productNameElement = document.querySelector(".entry h1");
+  let productGallery = document.querySelector(".wrap .swiper-wrapper");
+  let productGallerySmall = document.querySelector(".small");
+  let buttonColors = document.querySelector(".button__colors");
+  let productDescriptionElement = document.querySelector(".description p");
+  let productDetailsList = document.querySelector(".product__details");
 
+  // Product info
   productNameElement.innerText = product.productName;
 
-  let productImage = document.createElement("img");
-  productImage.src = product.mainImage;
-  productGalleryElement.appendChild(productImage);
+  let imageGallery = product.imageGallery;
 
-  productDescriptionTextElement.innerText = product.imageDescription;
-
-  let buyButton = document.createElement("button");
-  buyButton.innerText = "Comprar";
-  buyButton.addEventListener("click", function () {
-    window.location.href = `shop?id=${product.id}`;
+  imageGallery.forEach((image) => {
+    let swiperSlide = document.createElement("div");
+    let actualImage = document.createElement("img");
+    actualImage.src = image;
+    actualImage.alt = "";
+    swiperSlide.classList.add("item");
+    swiperSlide.classList.add("swiper-slide");
+    swiperSlide.appendChild(actualImage);
+    productGallery.appendChild(swiperSlide);
   });
-  productBuyElement.appendChild(buyButton);
 
-  updateBreadcrumb(product.productName);
+  imageGallery.forEach((image) => {
+    let itemImage = document.createElement("div");
+    let itemWrap = document.createElement("div");
+    let actualImage = document.createElement("img");
+    actualImage.src = image;
+    actualImage.alt = "";
+    itemImage.classList.add("item");
+    itemImage.classList.add("swiper-slide");
+    itemWrap.classList.add("thumb__wrap");
+    itemWrap.appendChild(actualImage);
+    itemImage.appendChild(itemWrap);
+    productGallerySmall.appendChild(itemImage);
+  });
+
+  let colors = product.colors;
+
+  colors.forEach((color, index) => {
+    let button = document.createElement("button");
+    button.classList.add("color__button");
+    button.style.backgroundColor = `var(--${color})`;
+
+    if (index == 0) {
+      button.classList.add("selected");
+    }
+
+    buttonColors.appendChild(button);
+  });
+
+  let allButtonColors = document.querySelectorAll(".color__button");
+
+  allButtonColors.forEach((button) => {
+    button.addEventListener("click", () => {
+      allButtonColors.forEach((button) => {
+        button.classList.remove("selected");
+      });
+
+      button.classList.add("selected");
+    });
+  });
+
+  let allSizeButtons = document.querySelectorAll(".size__button");
+
+  allSizeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      allSizeButtons.forEach((button) => {
+        button.classList.remove("selected");
+      });
+
+      button.classList.add("selected");
+    });
+  });
+
+  productDescriptionElement.innerText = product.description;
+
+  let productDetails = product.details;
+
+  productDetails.forEach((detail) => {
+    let unordenedList = document.createElement("li");
+
+    unordenedList.innerText = detail;
+
+    productDetailsList.appendChild(unordenedList);
+  });
+
+  const theadRow = document.querySelector("#tabela thead tr");
+  const larguraRow = document.querySelector("#larguraRow");
+  const alturaRow = document.querySelector("#alturaRow");
+  let sizes = product.sizes;
+
+  sizes.forEach((item) => {
+    const { size } = item;
+    const th = document.createElement("th");
+    th.textContent = size;
+    theadRow.appendChild(th);
+
+    const larguraTd = document.createElement("td");
+    larguraTd.textContent = item.width;
+    larguraRow.appendChild(larguraTd);
+
+    const alturaTd = document.createElement("td");
+    alturaTd.textContent = item.height;
+    alturaRow.appendChild(alturaTd);
+  });
+
+  disableMissingSizes(product);
+}
+
+function disableMissingSizes(product) {
+  const availableSizes = product.sizes.map((item) => item.size);
+  const allSizeButtons = document.querySelectorAll(".size__button");
+
+  allSizeButtons.forEach((button) => {
+    const size = button.textContent;
+    if (!availableSizes.includes(size)) {
+      button.disabled = true;
+    }
+  });
 }
 
 function updateBreadcrumb(productName) {
@@ -58,3 +154,72 @@ if (product) {
 } else {
   window.location.href = "erro.html";
 }
+
+// Product Gallery
+
+const thumbImage = new Swiper(".thumbnail__image", {
+  loop: true,
+  direction: "vertical",
+  spaceBetween: 15,
+  slidesPerView: 1,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const mainImage = new Swiper(".main__image", {
+  loop: true,
+  autoHeight: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  thumbs: {
+    swiper: thumbImage,
+  },
+});
+
+// Content box
+
+const tabs = document.querySelectorAll(".tab__btn");
+const all_content = document.querySelectorAll(".content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    if (window.innerWidth >= 820) {
+      let line = document.querySelector(".line");
+      line.style.width = e.target.offsetWidth + "px";
+      line.style.left = e.target.offsetLeft + "px";
+    } else {
+      let line = document.querySelector(".line");
+      line.style.height = e.target.offsetHeight + "px";
+      line.style.top = e.target.offsetTop + "px";
+    }
+
+    all_content.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    all_content[index].classList.add("active");
+  });
+});
+
+window.addEventListener("resize", () => {
+  tabs.forEach((tab, index) => {
+    if (tab.classList.contains("active")) {
+      const line = document.querySelector(".line");
+      if (window.innerWidth >= 820) {
+        line.style.width = tab.offsetWidth + "px";
+        line.style.left = tab.offsetLeft + "px";
+      } else {
+        line.style.height = tab.offsetHeight + "px";
+        line.style.top = tab.offsetTop + "px";
+      }
+    }
+  });
+});
