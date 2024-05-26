35ccacfbfbcdb74a7ef84004ede4f542db4ca632
higorini <108697425+higorini@users.noreply.github.com>
2024-05-26 20:38:59 +0100

    fix: vitrine
    Atualização da lógica da vitrine para ficar de acordo com as exigências do cliente.

---
+++ assets/js/vitrineImage.js
@@ -0,0 +1,37 @@
+let vitrineHome = {
+  data: [
+    {
+      imageName: "1-A.jpg",
+    },
+    {
+      imageName: "1-B.jpg",
+    },
+    {
+      imageName: "2-A.jpg",
+    },
+    {
+      imageName: "3-A.jpg",
+    },
+    {
+      imageName: "4-A.jpg",
+    },
+    {
+      imageName: "4-B.jpg",
+    },
+    {
+      imageName: "5-A.jpg",
+    },
+    {
+      imageName: "5-B.jpg",
+    },
+    {
+      imageName: "7-A.jpg",
+    },
+    {
+      imageName: "8-A.jpg",
+    },
+    {
+      imageName: "8-B.jpg",
+    },
+  ],
+};
